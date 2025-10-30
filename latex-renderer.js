324719fa7a32b74c70958f9734c91f39c0a0ed64
N. Adhikary <nil_adhikary@outlook.com>
2025-10-31 00:15:16 +0100

    Create latex-renderer.js for rendering LaTeX documents in textmode

---
+++ latex-renderer.js
@@ -0,0 +1,12 @@
+// latex-renderer.js
+// Rendering LaTeX documents in textmode
+
+// Title: LATEX
+
+function renderLatex(document) {
+    // Function to render LaTeX
+    console.log('Rendering LaTeX document:', document);
+    // Add your rendering logic here
+}
+
+module.exports = renderLatex;
